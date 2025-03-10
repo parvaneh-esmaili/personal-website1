ecbd01ebd8ecafb4f6fe087ce43cceb8bd49cdbc
Parvaneh esmaili <parvanehun@gmail.com>
2025-03-10 11:15:35 -0700

    INIT: Add website model

--- src/app/models/website.model.ts
+++ src/app/models/website.model.ts
@@ -1,2 +1,37 @@
 export class Website {
+    Name: string;
+    Email: string;
+    Message: string;
+
+    constructor(
+        Name: string = '',
+        Email: string = '',
+        Message: string = ''
+    ){
+        this.Name = Name;
+        this.Email = Email;
+        this.Message = Message;
+    }
 }
+
+export class WebsiteForLoad{
+    id: number;
+    Name: string;
+    Email: string;
+    Message: string;
+
+    constructor(
+        id: number= 0 ,
+        Name : string ='',
+        Email: string= '',
+        Message: string= ''
+        ){
+            this.id = id;
+            this.Name = Name;
+            this.Email = Email;
+            this.Message = Message;
+        }
+   
+}
+
+
